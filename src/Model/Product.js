9bde0a6194cde5f6df13df6e25e0da8a16ac12fa
kim min seok <nas7062@naver.com>
2024-11-11 14:42:27 +0900

    feat(Product) : Product 관리하는 클래스

--- src/Model/Product.js
+++ src/Model/Product.js
@@ -1,5 +1,4 @@
 class Product {
-
   constructor(name, price, quantity, promotion, promotionCount) {
     this.name = name,
       this.price = price,
@@ -7,8 +6,6 @@ class Product {
       this.promotion = promotion,
       this.promotionCount = promotionCount;
   }
-
 }
-
 
 export default Product;
